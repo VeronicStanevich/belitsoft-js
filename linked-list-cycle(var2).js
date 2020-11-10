77a223357467877943d3b4b58064d27db00a4ba0
veranika <veronica.stanevich@gmail.com>
2020-11-10 13:09:09 +0300

    add linked-list-cycle

---
+++ linked-list-cycle(var2).js
@@ -0,0 +1,17 @@
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const map = new Map();
+    let current = head;
+    while(current){
+        if(!map.has(current)){
+            map.set(current);
+        }else{
+            return current;
+        }
+        current = current.next;
+    }
+    return false;
+};
