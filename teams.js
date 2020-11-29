17446031aa1cc8dabd0f03e18b042d161ae3d51f
veranika <veronica.stanevich@gmail.com>
2020-11-29 14:43:03 +0300

    Updates

--- teams.js
+++ teams.js
@@ -1,27 +1,31 @@
 (function () {
     'use strict';
 
-    const headers = new Headers();
-    headers.append('X-Auth-Token', 'f113f557ce2a428e8a46dc9e659212f8');
     document.getElementById('refresh-button').addEventListener('click', getTeams)
 
-    function getTeams() {
-        fetch('https://api.football-data.org/v2/teams', {
+    function fetchTeams() {
+        const headers = new Headers();
+        headers.append('X-Auth-Token', 'f113f557ce2a428e8a46dc9e659212f8');
+
+        return fetch('https://api.football-data.org/v2/teams', {
             method: 'GET',
             headers: headers
         })
             .then(res => res.json())
-            .then(responseJSON => {
-                function generateHTML(team) {
-                    return `<div class="team__card">
+    }
 
+    function generateHTML(team) {
+        return `<div class="team__card">
                       <a class="team__link" href="matches.html?id=${team.id}">
                         <img class="team__logo" alt="${team.name}" src="${team.crestUrl}" />
                         <p>${team.name}</p>
                       </a>
-                    </div>`;
-                }
+                </div>`;
+    }
 
+    function getTeams() {
+        fetchTeams()
+            .then(responseJSON => {
                 const root = document.querySelector(".teams");
                 responseJSON.teams.forEach((team) => {
                     root.innerHTML += generateHTML(team);
@@ -31,5 +35,4 @@
     }
 
     getTeams();
-
 })();
